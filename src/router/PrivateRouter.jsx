34ed0366c334669df16b40edc53a84b842fd94e2
ubing <ubing0101@kakao.com>
2025-05-13 15:51:06 +0900

    router 기초 설정 - 추후 라우터 기능 업데이트 필요

---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrivateRouter = () => {
+  return (
+    <div>
+      로그인사용자만 받을수있는 프라이빗 라우터 추후 로그인 기능 구현 후 작업
+      진행
+    </div>
+  );
+};
+
+export default PrivateRouter;
