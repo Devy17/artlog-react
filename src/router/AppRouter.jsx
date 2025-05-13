cee7fdee129eb51043fde5b0280c0057adf0acd2
ubing <ubing0101@kakao.com>
2025-05-13 17:51:37 +0900

    로그인 - 미완성

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import Home from '../Component/Home';
 import { Route, Routes } from 'react-router-dom';
+import PrivateRouter from './PrivateRouter';
+import Home from '../Component/Home';
+import SignInPage from '../Page/user/SignInPage';
 
 const AppRouter = () => {
   return (
     <Routes>
       {/* 임의의 Home 개설 메인페이지 담당하시는 분은 Home 제거 후 메인페이지 연결해주세요 */}
       <Route path='/' element={<Home />} />
+      {/* 로그인 토큰 테스트 */}
+      <Route
+        path='/test'
+        element={<PrivateRouter element={<SignInPage />} />}
+      />
+      <Route path='/login' element={<SignInPage />} />
     </Routes>
   );
 };
