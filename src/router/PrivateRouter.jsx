ec7fd4eea1aabc58ed9b1123902e44236efa86fb
ubing <ubing0101@kakao.com>
2025-05-14 19:13:29 +0900

    쿠폰등록 및 조회

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -11,14 +11,14 @@ const PrivateRouter = ({ element, requiredRole }) => {
   if (!isInit) return <div>Loading...</div>;
 
   if (!isLoggedIn) {
-    alert('로그인 안함!');
+    alert('회원만 이용가능한 페이지입니다. 로그인을 해주세요!');
     // to=보내고 싶은 페이지 렌더링 주소
     // replace = 사용자가 뒤로가기 버튼을 눌러도 이전 페이지로 돌아가지 않게 됨.
     return <Navigate to='/login' replace />;
   }
 
   if (requiredRole && userRole !== requiredRole) {
-    alert('권한 없음!');
+    alert('권한이 없습니다!');
     return <Navigate to='/' replace />;
   }
 
