a98a232869646ebaf1936ea32187d82535f5ad6b
Devy17 <devyks107@gmail.com>
2025-05-15 14:38:25 +0900

    fix

--- src/Axios/host-config.js
+++ src/Axios/host-config.js
@@ -4,12 +4,9 @@ let backendHostName;
 
 if (clientHostName === 'localhost') {
   backendHostName = 'http://localhost:8000';
-} else if (
-  clientHostName ===
-  'artlog-s3-instance-front-21974.s3-website.ap-northeast-2.amazonaws.com'
-) {
+} else {
   // 배포해서 현재 서비스 중
-  backendHostName = '43.201.55.136:8000';
+  backendHostName = 'http://43.201.55.136:8000';
 }
 
 export const API_BASE_URL = backendHostName;
